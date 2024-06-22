506f447cb07caeceaad1dcde1fae76c284de2e3b
Ashfaq Ahmed <ashfaqdotes@gmail.com>
2024-06-23 02:01:48 +0500

    update all files
    updates to latest and greatest

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
-let userInput = document.getElementById("input")
-let addTodo = document.getElementById("input-btn")
-let makeList = document.getElementById("display")
+let userInput = document.getElementById("input");
+let makeList = document.getElementById("display");
 let counter = 0;
-addTodo.addEventListener('click', () => {
-  // console.log('Working')
+
+userInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    addTodo();
+  }
+});
+
+function addTodo() {
   if (userInput.value !== '') {
-    makeList.innerHTML += `<li>${userInput.value}
-    <button id='${counter}' onclick='toEdit("${counter}")'><span class="material-symbols-outlined">edit</span></button>
-    <button id='${counter}' onclick='toDelete("${counter}")'><span class="material-symbols-outlined">delete</span></button></li>`
+    let newItem = document.createElement('li');
+    newItem.setAttribute('id', `item-${counter}`);
+    newItem.innerHTML = `
+      <input type="checkbox" id="checkbox-${counter}" onchange="toggleStrikeThrough('${counter}')">
+      <span id="text-${counter}">${userInput.value}</span>
+      <div>
+        <button id="edit-btn-${counter}" onclick='toEdit("${counter}")'>
+          <span class="material-symbols-outlined">edit</span>
+        </button>
+        <button onclick='toDelete("${counter}")'>
+          <span class="material-symbols-outlined">delete</span>
+        </button>
+      </div>
+    `;
+    makeList.prepend(newItem);
     userInput.value = '';
-    counter++
+    counter++;
+    makeList.scrollTop = 0;
+  } else {
+    alert('Please Enter Something to Add Todo');
   }
-  else {
-    alert('Please Enter Something to Add Todo')
-  }
-})
+}
+
+function toggleStrikeThrough(id) {
+  let item = document.getElementById(`item-${id}`);
+  let editButton = document.getElementById(`edit-btn-${id}`);
+  item.classList.toggle('strikethrough');
+  editButton.disabled = !editButton.disabled;
+}
 
 function toEdit(id) {
-  let itemToEdit = document.getElementById(id)
-  itemToEdit.parentNode.firstChild.textContent = prompt('Type to Edit: ')
+  let itemText = document.getElementById(`text-${id}`);
+  let newText = prompt('Type to Edit:', itemText.textContent);
+  if (newText !== null && newText.trim() !== '') {
+    itemText.textContent = newText;
+  }
 }
 
 function toDelete(id) {
-  let itemToDelete = document.getElementById(id)
-  itemToDelete.parentNode.parentNode.remove()
+  let itemToDelete = document.getElementById(`item-${id}`);
+  itemToDelete.remove();
+}
 
+function clearAllTodos() {
+  makeList.innerHTML = '';
 }
